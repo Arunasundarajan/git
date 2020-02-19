732c03fd2df5291b6943b986e5b7f1a450811175
aruna <aruna.tkspys@gmail.com>
2020-02-19 16:02:48 +0530

    login

---
+++ src/test/LoginTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+   @Test
+    public void signin()
+   {
+       System.setProperty("Webdriver.chrome.driver","C:\\Users\\PREMANAND RAJA\\Desktop\\Selenium notes\\Project1\\Driver\\chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+       driver.get("https://www.spicejet.com/");
+       String Title= driver.getTitle();
+       System.out.println(Title);
+   }
+
+}
