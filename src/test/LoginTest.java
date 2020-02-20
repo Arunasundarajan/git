1812e8c7bdd287f3d0a471c36ff9ac6501d0aed4
aruna <aruna.tkspys@gmail.com>
2020-02-20 15:49:39 +0530

    webpage open

--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -6,14 +6,15 @@ import org.testng.annotations.Test;
 
 public class LoginTest {
 
-   @Test
+    @Test
     public void signin()
-   {
-       System.setProperty("Webdriver.chrome.driver","C:\\Users\\PREMANAND RAJA\\Desktop\\Selenium notes\\Project1\\Driver\\chromedriver.exe");
-       WebDriver driver = new ChromeDriver();
-       driver.get("https://www.spicejet.com/");
-       String Title= driver.getTitle();
-       System.out.println(Title);
-   }
+    {
+        //open a webpage
+        System.setProperty("Webdriver.chrome.driver","C:\\Users\\PREMANAND RAJA\\Desktop\\Selenium notes\\Project1\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.spicejet.com/");
+        String Title= driver.getTitle();
+        System.out.println(Title);
+    }
 
 }
